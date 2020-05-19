1dd02780bffa4e57e285bdd447c758acb8d70fc6
sa <ctrlc@users.noreply.github.com>
2020-05-19 11:21:56 +0800

    增加“出库并更新库存”功能
    增加“出库并更新库存”功能

--- src/main/java/com/byteframework/psi/service/SaleService.java
+++ src/main/java/com/byteframework/psi/service/SaleService.java
@@ -1,8 +1,8 @@
 package com.byteframework.psi.service;
 
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.byteframework.commons.exception.CustomException;
 import com.byteframework.psi.domain.Sale;
 
 /**
@@ -21,4 +21,18 @@ public interface SaleService extends IService<Sale> {
      * @return
      */
     IPage<Sale> listSale(IPage page, Sale sale);
+
+
+    /**
+     * 保存销售信息
+     * <p>
+     * 流程：
+     * 1. 保存销售信息
+     * 2. 根据销售的产品编号查询库存信息
+     * 3. 更新库存
+     * </p>
+     *
+     * @return
+     */
+    void saveSale(Sale sale) throws CustomException;
 }
